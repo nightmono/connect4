e7b3434b44397a5c0f8f1acc8549e3a5022bb3d7
nightmono <163682003+nightmono@users.noreply.github.com>
2024-07-25 14:02:29 +0100

    v1.0.0

--- connect4.py
+++ connect4.py
@@ -1,77 +1,70 @@
 #!/usr/bin/python3
 
-board = [["." for _ in range(7)] for _ in range(6)]
-
 def print_board(board: list[list[str]]):
     for row in board:
-        print(" ".join(row))
+        print(" ".join(row).replace(".", "--").replace("X", "\033[1;31m██\033[0m").replace("O", "\033[1;33m██\033[0m"))
 
 def in_bounds(x: int, y: int) -> bool:
-    if x < 0 or x > 6:
-        return False
-    if y < 0 or y > 5:
-        return False
-    return True
+    return x >= 0 and x <= 6 and y >= 0 and y <= 5
 
 def place_piece(board, piece, x):
     if not in_bounds(x, 0):
         print("Out of bounds")
+        return -1, -1
 
     for y in range(5, -1, -1):
         if board[y][x] == ".":
             board[y][x] = piece
-            print(f"Placed at {x},{y}")
             return x, y
     
     print("Column full")
+    return -1, -1
 
-def check_direction(board, piece, x1, x2, y1, y2):
-    # Currently only workds for verticals and horizontals
-    # Diagonal checks like a 5x5 sqaure for some reason
-    # One potential fix is by using a while loop and a xstep and ystep 
-    # argument as well
-
-    # x = x1
-    # y = y1
-    # while x <= x2 and y <= y2:
-    #   ...
-    #   x += xstep
-    #   y += ystep
-
+def check_direction(board, piece, x1, x2, xstep, y1, y2, ystep):
     num = 0
 
-    for y in range(y1, y2+1):
-        for x in range(x1, x2+1):
-            if not in_bounds(x, y):
-                continue
-            elif board[y][x] == piece:
-                num += 1
-            else:
-                num = 0
+    x = x1
+    y = y1
+    while x != x2 and y != y2:
+        if not in_bounds(x, y):
+            pass
+        elif board[y][x] == piece:
+            num += 1
+        else:
+            num = 0
 
-            print(x, y)
+        if num == 4:
+            return True
 
-            if num == 4:
-                return True
-    
+        x += xstep
+        y += ystep
+
     return False
 
 def check_win(board, piece, x, y):
-    """
-    return (check_direction(board, piece, x-3, x+3, y, y) or
-            check_direction(board, piece, x, x, y-3, y+3) or
-            check_direction(board, piece, x-3, x+3, y-3, y+3) or
-            check_direction(board, piece, x+3, x-3, y-3, y+3))
-    """
-    return check_direction(board, piece, x-3, x+3, y-3, y+3)
+    return (check_direction(board, piece, x-3, x+4, 1, y, y+1, 0) or
+            check_direction(board, piece, x, x+1, 0, y-3, y+4, 1) or
+            check_direction(board, piece, x-3, x+4, 1, y-3, y+4, 1) or
+            check_direction(board, piece, x-3, x+4, 1, y+3, y-4, -1))
 
-def main():
-    while 1:
+def start_game():
+    board = [["." for _ in range(7)] for _ in range(6)]
+    game_running = True
+    
+    while game_running:
         for piece in ("X", "O"):
             print_board(board)
             col = int(input("> "))
             x, y = place_piece(board, piece, col)
-            print(check_win(board, piece, x, y))
+            if check_win(board, piece, x, y):
+                print(f"Player {piece} wins!")
+                print_board(board)
+                game_running = False
+                break
+            print()
+
+def main():
+    start_game()
 
 if __name__ == "__main__":
     main()
